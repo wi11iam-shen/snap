832598d341d6d89ad83598332c167853060a9d17
William Shen <william.shen.0001@gmail.com>
2024-04-09 19:39:07 -0700

    final product part 1 consider adding more

--- DisplayInformation/scripts.js
+++ DisplayInformation/scripts.js
@@ -3,8 +3,11 @@ import data from "./data.json" with {type: "json"};
 const database = data;
 let current_dog = "";
 let current_cat = "";
-const favorite_dogs = []
-const favorite_cats = []
+let dog_url = "";
+let cat_url = "";
+const favorite_dogs = {};
+const favorite_cats = {};
+
 
 //When reloaded add images ins
 addEventListener("DOMContentLoaded", reload);
@@ -13,6 +16,7 @@ document.getElementById("refresh").addEventListener("click", reload);
 //Add to favorite functions for cat and dog buttons
 document.getElementById("dog-button").addEventListener("click", click_dog);
 document.getElementById("cat-button").addEventListener("click", click_cat);
+
 
 
 //Grab a random image from one of the two api's depending on the button clicked
@@ -24,8 +28,11 @@ async function randomDogImage( breed){
         //Add a function to input the image into the proper container
         const img = document.createElement("img");
         img.src = data["message"];
-        img.style.width = "300px";
-        img.style.height = "300px";
+        dog_url = img.src;
+        img.style.width = "400px";
+        img.style.height = "400px";
+        img.style.borderRadius = "22px";
+        img.style.objectFit = "cover";
         const dogSrc = document.getElementById("dogcard");
         if(dogSrc.childNodes.length > 0){
             dogSrc.removeChild(dogSrc.lastChild);
@@ -47,8 +54,11 @@ async function randomCatImage( breed){
         const data = await response.json();
         const img = document.createElement("img");
         img.src = data[0]["url"];
-        img.style.width = "300px";
-        img.style.height = "300px";
+        cat_url = img.src;
+        img.style.width = "400px";
+        img.style.height = "400px";
+        img.style.borderRadius = "22px";
+        img.style.objectFit = "cover";
         const catSrc = document.getElementById("catcard");
         if(catSrc.childNodes.length > 0){
             catSrc.removeChild(catSrc.lastChild);
@@ -69,7 +79,8 @@ function randomBreed(count, type){
         current_dog = dog_array;
         return dog_array;
     } else{
-        current_cat = database["cats"][random_breed]['name'];
+        let cat_array = database["cats"][random_breed];
+        current_cat = cat_array;
         return database["cats"][random_breed]["id"];
         
     }
@@ -86,16 +97,52 @@ function reload(){
 
 }
 
+
+
 function show(animal_list, location){
   const parent = document.getElementById(location);
   removeAllChildNodes(parent);
-  if(animal_list.length >0){
+  if(Object.keys(animal_list).length >0){
   for (let animal in animal_list){
-    const new_fav = document.createElement("li"); 
-    new_fav.appendChild(document.createTextNode(animal_list[animal]));
+    const new_fav = document.createElement("span");
+    new_fav.id = "span-card";
+    const animal_node = document.createElement("span");
+    animal_node.className = "name-card";
+    if(location == "dog-list"){
+        animal_node.appendChild(document.createTextNode(animal));
+    }
+    else{
+        animal_node.appendChild(document.createTextNode(animal_list[animal]));
+    }
+    
+    //Create a button for removal
+    const button = document.createElement("button");
+    button.id = "remove-button";
+    button.className = location;
+    button.appendChild(document.createTextNode("Remove"));
+    //Change the animal name in the spa 
+    new_fav.appendChild(animal_node);
+    new_fav.appendChild(button);
     parent.appendChild(new_fav);
   }
 } 
+    parent.addEventListener("click", (e) =>{
+        //console.log(e.target.parentNode.firstChild);
+        if(e.target.tagName == "BUTTON"){
+            
+            if(e.target.className == "dog-list"){
+                let index = e.target.parentNode.firstChild.innerText;
+                delete favorite_dogs[index];
+                console.log(favorite_dogs);
+            }else{
+                let index = e.target.parentNode.firstChild.innerText;
+                let target_key = Object.keys(favorite_cats, index).filter((key) => favorite_cats[key] == index);
+                delete favorite_cats[target_key];
+            }
+            e.target.parentNode.remove();
+        }
+        
+    }, false)
 }
 
 function removeAllChildNodes(parent) {
@@ -107,22 +154,18 @@ function removeAllChildNodes(parent) {
 
 function click_dog(){
       //Check if the breed exists in the list already
-      if(favorite_dogs.includes(current_dog)){
+      if(current_dog in favorite_dogs){
         alert("You already have this type of dog saved");
-      } else{
-        favorite_dogs.push(current_dog);
-        console.log(favorite_dogs);
+      } else{   
+        favorite_dogs[current_dog] = dog_url;
         show(favorite_dogs, "dog-list");
       }
     }
 function click_cat(){
-      if(favorite_cats.includes(current_cat)){
+      if(current_cat["id"] in favorite_cats){
         alert("You already have this type of cat saved");
       }else{
-        favorite_cats.push(current_cat);
+        favorite_cats[current_cat["id"]] = current_cat["name"];
         show(favorite_cats, "cat-list");
       }
   }
-
-
-
